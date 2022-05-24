456586c4398055a98c50fb6c7c4da8f9bee2de35
Crislâine Santos <crislaine.costa@arapiraca.ufal.br>
2022-05-24 11:12:39 -0300

    Create MappingsStructExample.sol

---
+++ mappings/MappingsStructExample.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.5.13;
+
+contract MappingsStructExample {
+
+    mapping(address => uint) public balanceReceived;
+
+    function getBalance() public view returns(uint) {
+        return address(this).balance;
+    }
+
+    function sendMoney() public payable {
+        balanceReceived[msg.sender] += msg.value;
+    }
+
+    function withdrawAllMoney(address payable _to) public {
+        uint balanceToSend = balanceReceived[msg.sender];
+        balanceReceived[msg.sender] = 0;
+        _to.transfer(balanceToSend);
+    }
+}

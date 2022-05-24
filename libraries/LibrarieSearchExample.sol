4f2a71313e9a058c5ce9875705336d07d671d875
Crislâine Santos <crislaine.costa@arapiraca.ufal.br>
2022-05-24 16:45:33 -0300

    Add UsingForExample Contract

--- libraries/LibrarieSearchExample.sol
+++ libraries/LibrarieSearchExample.sol
@@ -1,6 +1,4 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >= 0.4.16 < 0.7.0;
-
+pragma solidity >= 0.4 .16 < 0.7 .0;
 library Search {
     function indexOf(uint[] storage self, uint value)
     public
@@ -11,6 +9,26 @@ library Search {
         return uint(-1);
     }
 }
+
+contract UsingForExample {
+    using Search
+    for uint[];
+    uint[] data;
+
+    function append(uint value) public {
+        data.push(value);
+    }
+
+    function replace(uint _old, uint _new) public {
+        // This performs the library function call
+        uint index = data.indexOf(_old);
+        if (index == uint(-1))
+            data.push(_new);
+        else
+            data[index] = _new;
+    }
+}
+
 contract NotUsingForExample {
     uint[] data;
 
